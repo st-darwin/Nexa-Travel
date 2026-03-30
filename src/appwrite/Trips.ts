e3d91854393ed375e25ba87b7fa1b14e2dce0824
Darwin_10x <uzomasolomon289@gmail.com>
2026-03-30 10:17:44 +0100

    Add files via upload

---
+++ src/appwrite/Trips.ts
@@ -0,0 +1,40 @@
+import { Query } from "appwrite"
+import { appwriteConfig , database } from "./client"
+
+export const getALlTrips = async(limit : number , offset : number) =>{
+    const allTrips = await  database.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.tripCollectionId,
+         [Query.limit(limit) , Query.offset(offset) , Query.orderDesc('$createdAt')]
+
+    )
+    if(!allTrips.total){
+        return {
+            allTrips : [],
+            total : 0
+        }
+    }
+    return{
+        allTrips : allTrips.documents,
+        total : allTrips.total
+    }
+   
+
+    
+}
+
+export const getTripById = async(tripId : string) =>{
+    const trip = await database.getDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.tripCollectionId,
+        tripId,
+    )
+    if(!trip.$id){
+        console.log('Trip not found')
+        return null;
+    }
+    return trip;
+
+
+}
+
