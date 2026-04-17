f7d95427bc5d86bef77730a306c1711cb5db3761
darwin <uzomasolomon289@gmail.com>
2026-04-17 23:04:28 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,48 @@
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
-import Dashboard from "./sections/admin/Dashboard";
+import Dashboard , {DashboardLoader as AdminLoader }  from "./sections/admin/Dashboard";
 import AdminLayout, { clientLoader as adminLoader } from "./sections/admin/AdminLayout";
 import TripDetails , {loader as tripDetailsLoader} from "./sections/admin/TripDetails";
 import SignIn, { clientLoader as signInLoader } from "./sections/root/sign-in";
 import AllUser, { loader as allUserLoader } from './sections/admin/AllUser'; // Import the loader!
-import Trips from "./sections/admin/Trips";
+import Trips ,  {loader as TripsLoader } from "./sections/admin/Trips";
 import CreateTrips , {loader as createTripsLoader} from "./sections/admin/CreateTrips";
+import Logout from "./sections/admin/Logout";
 
  // Import your action
  import { action as createTripAction } from "./sections/api/AICreateTrip";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      {/* Sign In Route: If logged in, this loader kicks you to Dashboard */}
-      <Route 
-        path="/sign-in" 
-        element={<SignIn />} 
-        loader={signInLoader} 
-      />
-
-      {/* API ROUTE TS */ }
-
-
-
-      
-    
-
-      {/* Admin Protected Routes: If NOT logged in, this loader kicks you to Sign-In */}
-      <Route 
-        element={<AdminLayout />} 
-        loader={adminLoader}
-      > 
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/all-users" loader={allUserLoader} element={<AllUser />} />
-        <Route path="/trips" element={<Trips/>}/>
-        <Route path="/trips/create" loader={createTripsLoader}
-        action= {createTripAction}
-        element={<CreateTrips/>}/>
-     
-      <Route 
-          path="/trips/:id" 
-          element={<TripDetails />} 
-          loader={tripDetailsLoader}
+      {/* Admin Protected Group */}
+      <Route element={<AdminLayout />} loader={adminLoader}>
+        {/* Use 'index' for the root of the protected group */}
+        <Route index element={<Dashboard />} loader={AdminLoader} />
+        <Route path="all-users" element={<AllUser />} loader={allUserLoader} />
+        <Route path="trips" element={<Trips />} loader={TripsLoader} />
+        <Route 
+          path="trips/create" 
+          element={<CreateTrips />} 
+          loader={createTripsLoader} 
+          action={createTripAction} 
         />
-         </Route>
+        <Route 
+          path="trips/:id" 
+          element={<TripDetails />} 
+          loader={tripDetailsLoader} 
+        />
+        <Route path="/logout" element={<Logout />} />
+      </Route>
+
+      {/* Public Routes - Kept outside the AdminLayout guard */}
+      
+      <Route path="/sign-in" element={<SignIn />} loader={signInLoader} />
     </>
   )
 );
 
 function App() {
-  return <RouterProvider router={router}  />;
+  return <RouterProvider router={router}  fallbackElement={<div className="h-screen bg-slate-50" />} />;
 }
 
 export default App;
