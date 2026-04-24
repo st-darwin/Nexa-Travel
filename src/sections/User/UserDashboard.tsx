a937f3e2d71a440100eab7c1f6f4966b5b008c8b
darwin <uzomasolomon289@gmail.com>
2026-04-24 10:24:44 +0100

    Implement user data loading in UserDashboard
    Added user data loading and header display in UserDashboard.

--- src/sections/User/UserDashboard.tsx
+++ src/sections/User/UserDashboard.tsx
@@ -1,10 +1,32 @@
+// i am too lazyy brufff
+import { useLoaderData } from "react-router-dom";
+import { getUser } from "../../appwrite/Auth"
+import { Header } from "../../components/Index";
 
+export const UserDashboradLoader = async () =>{
+ try{
+   const user = await getUser()
+ 
+   return user ;
+  
+  }
+
+  catch(e){
+    console.log( e , "Unable to get user data in the user dashborad")
+  }
+  
+
+}
 
 const UserDashboard = () => {
+  const user = useLoaderData() as any;
   return (
-    <div>
-      DashBorad 
-    </div>
+     <>
+     <Header
+     title={user.$id ? `Welcome ${user.name} 👋` : "Welcome Guest 💕"}
+     description="View and mnange ready-made itineraries"
+     />
+     </>
   )
 }
 
